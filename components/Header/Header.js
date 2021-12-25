e520cfb74c928461f7ed2e7e4309fda11185d3a6
Skendoul Mohamed <skendoulmohamed@gmail.com>
2021-12-25 01:48:11 +0100

    first commit

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -7,14 +7,10 @@ import { TiThMenu } from "@react-icons/all-files/ti/TiThMenu";
 
 
 const Header = () => {
-  const [Url, setUrl] = useState(
-    "/"
-  );
-  useEffect(() => {
-    const landed = true;
-  });
-  console.log("");
-
+  const [Hide, setHide] = useState(false)
+  const HideMenu = ()=>{
+    setHide(!Hide);
+  }
   return (
     <>
       <LiveCHat />
@@ -33,43 +29,46 @@ const Header = () => {
           ></div>
         </Link>
         <div className={Style.div_nav_bar_mobile_icon}>
-          <TiThMenu />
+          <TiThMenu onClick={HideMenu} />
         </div>
-        <div className={Style.div_nav_bar}>
-          <ul className={Style.ul_nav_bar}>
-            <li className={Style.li_nav_bar}>
-              <Link href={`${Url}`}>Home</Link>
-            </li>
-            <li className={Style.li_nav_bar}>
-              <Link href="/tours-and-circuits/tours-from-marrakech/Fmmf">
-                Tours & Cuircuits
-              </Link>
-            </li>
-            <li className={Style.li_nav_bar}>
-              <Link href="/">Excursions</Link>
-            </li>
-            <li className={Style.li_nav_bar}>
-              <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
-                About us
-              </Link>
-            </li>
-            <li className={Style.li_nav_bar}>
-              <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
-                Contact us
-              </Link>
-            </li>
-            <li className={Style.li_nav_bar}>
-              <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
-                FaQ
-              </Link>
-            </li>
-            <li className={Style.li_nav_bar}>
-              <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
-                Blog
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {Hide ? (
+          <div className={Style.div_nav_bar}>
+            <ul className={Style.ul_nav_bar}>
+              X
+              <li className={Style.li_nav_bar}>
+                <Link href="/" passHref={true}>Home</Link>
+              </li>
+              <li className={Style.li_nav_bar}>
+                <Link href="/tours-and-circuits/tours-from-marrakech/Fmmf">
+                  Tours & Cuircuits
+                </Link>
+              </li>
+              <li className={Style.li_nav_bar}>
+                <Link href="/">Excursions</Link>
+              </li>
+              <li className={Style.li_nav_bar}>
+                <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
+                  About us
+                </Link>
+              </li>
+              <li className={Style.li_nav_bar}>
+                <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
+                  Contact us
+                </Link>
+              </li>
+              <li className={Style.li_nav_bar}>
+                <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
+                  FaQ
+                </Link>
+              </li>
+              <li className={Style.li_nav_bar}>
+                <Link href="http://${hostname}/tours-and-circuits/tours-from-marrakech/Fmmf">
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+        ) : null}
       </div>
       <div></div>
     </>
