7b36adb8ed0c4ca39fd279eb051bad6c774210a8
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-01-11 20:47:35 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../layout/Layout";
-// import { createStore } from "redux";
-// import { withRedux } from "next-redux-wrapper";
-// import { rooteReducer } from "../redux/rooteReducer";
-// import { provider } from "react-redux";
-// import { store } from "../redux/store";
+store";
 
 // const store = createStore(rooteReducer);
 function MyApp({ Component, pageProps }) {
@@ -16,7 +12,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 
-// const makeStore = () => store;
  export default MyApp
-
-// export default withRedux(makeStore)(MyApp);
