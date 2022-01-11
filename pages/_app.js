e2e7bfa3253f83bf3f4a889693703821c1309971
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-01-11 20:54:53 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../layout/Layout";
-store";
 
 // const store = createStore(rooteReducer);
 function MyApp({ Component, pageProps }) {
